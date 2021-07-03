3c2c13b6044a865e3cdab33ed8fc2a7ce128ba2f
cristina <cristina.outeda.rua@gmail.com>
2021-07-03 18:15:37 +0200

    added createStage to tetris view

--- src/Views/Tetris/Tetris.js
+++ src/Views/Tetris/Tetris.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 // components
-
+import { createStage } from "../../helpers";
 import { Stage, Display, StartButton } from "../../components";
 
 export function Tetris() {
-    return (
+  console.log("en tetris", createStage());
+  return (
+    <div>
+      <Stage stage={createStage()} />
+      <aside>
         <div>
-            <Stage/>
-            <aside>
-                <div>
-                    <Display text = "score" />
-                    <Display text = "Rows" />
-                    <Display text = "Level" />
-                </div>
-                <StartButton/>
-            </aside>
+          <Display text="score" />
+          <Display text="Rows" />
+          <Display text="Level" />
         </div>
-    )
+        <StartButton />
+      </aside>
+    </div>
+  );
 }
