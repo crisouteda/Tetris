70c710ae5d2c65c903bd8bf9e334abcc6fe9b323
cristina <cristina.outeda.rua@gmail.com>
2021-07-04 19:35:45 +0200

    added background onNew bestScore

--- src/components/Display/Style.js
+++ src/components/Display/Style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledDisplay = styled.div`
   box-sizing: border-box;
   display: flex;
-  align-items: center;
+  justify-content: center;
   margin: 0 0 20px 0;
   padding: 20px;
   border: 4px solid #333;
@@ -11,7 +11,7 @@ export const StyledDisplay = styled.div`
   width: 100%;
   border-radius: 20px;
   color: ${(props) => (props.gameOver ? "red" : "#999")};
-  backoground: #000;
+  background: ${(props) => (props.newBestScore ? "green" : "#000")};
   font-family: Pixel, Arial, Helvetica, sans-serif;
   font-size: 0.8rem;
 `;
