efe1c7a2b1eaf41a6354d7ce29552e7f8cccc282
cristina <cristina.outeda.rua@gmail.com>
2021-07-04 14:18:39 +0200

    added rotation functionality

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,10 @@
 import { useState, useCallback } from "react";
-import { TETROMINOS, randomTetromino, STAGE_WIDTH } from "../helpers";
+import {
+  TETROMINOS,
+  randomTetromino,
+  STAGE_WIDTH,
+  checkCollision,
+} from "../helpers";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -7,6 +12,34 @@ export const usePlayer = () => {
     tetromino: TETROMINOS[0].shape,
     collided: false,
   });
+
+  const rotate = (matrix, dir) => {
+    // Make the rows to become cols (transpose)
+    const rotateTetro = matrix.map((_, index) =>
+      matrix.map((col) => col[index])
+    );
+    // Reverse each row to get a rotated matrix
+    if (dir > 0) return rotateTetro.map((row) => row.reverse());
+    return rotateTetro.reverse();
+  };
+
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + offset > 0 ? 1 : -1);
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+    setPlayer(clonedPlayer);
+  };
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer((prev) => ({
@@ -24,5 +57,5 @@ export const usePlayer = () => {
     });
   });
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
