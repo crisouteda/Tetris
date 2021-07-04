c5cfcf205494620d4918dbdfa9747333969c1f59
cristina <cristina.outeda.rua@gmail.com>
2021-07-04 18:25:10 +0200

    removed unused styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import React from "react";
-import {Tetris} from "./Views"
+import { Tetris } from "./Views";
 
 function App() {
   return (
     <div className="App">
-      <Tetris/>
+      <Tetris />
     </div>
   );
 }
