180612178363dfa7c77a3d3e5ef557952e4f1800
cristina <cristina.outeda.rua@gmail.com>
2021-07-04 19:35:03 +0200

    added backgound onNewBestScore

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { StyledDisplay } from "./Style";
-export function Display({ gameOver, text }) {
-  return <StyledDisplay>{text}</StyledDisplay>;
+export function Display({ gameOver, newBestScore, text }) {
+  return (
+    <StyledDisplay gameOver={gameOver} newBestScore={newBestScore}>
+      {text}
+    </StyledDisplay>
+  );
 }
