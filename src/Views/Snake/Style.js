c5fb7cfef53b8906c7a72bdddcc76a6b54140d9b
cristina <cristina.outeda.rua@gmail.com>
2021-07-09 21:19:39 +0200

    styled snake view

---
+++ src/Views/Snake/Style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledGameArea = styled.div`
+  position: relative;
+  width: 600px;
+  height: 600px;
+  background-color: #fff;
+`;
+
+export const SnakeDot = styled.div`
+  position: absolute;
+  width: 12px;
+  height: 12px;
+  background-color: #000;
+  border: 1px solid #fff;
+`;
